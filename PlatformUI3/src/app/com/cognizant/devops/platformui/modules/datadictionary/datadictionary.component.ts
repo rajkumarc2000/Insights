6001b88152caa0c37a242b42100b1b68e391b8be
gauravdeshmukh612 <gaurav.deshmukh2@cognizant.com>
2019-01-17 09:31:21 +0530

    163186239 - data dictionary optimization

--- PlatformUI3/src/app/com/cognizant/devops/platformui/modules/datadictionary/datadictionary.component.ts
+++ PlatformUI3/src/app/com/cognizant/devops/platformui/modules/datadictionary/datadictionary.component.ts
@@ -7,33 +7,34 @@ import { MatTableDataSource } from '@angular/material';
 @Component({
   selector: 'app-datadictionary',
   templateUrl: './datadictionary.component.html',
-  styleUrls: ['./datadictionary.component.css','./../home.module.css']
+  styleUrls: ['./datadictionary.component.css', './../home.module.css']
 })
 export class DatadictionaryComponent implements OnInit {
-  dictResponse:any;
+  dictResponse: any;
   agentDataSource = [];
-  agentNodes=[];
+  agentNodes = [];
   displayedAgentColumns: string[];
   selectedAgent1: any;
-  agent1TableData:any;
-  agent2TableData:any;
+  selectedAgent2: any;
+  agent1TableData: any;
+  agent2TableData: any;
   readChange: boolean = false;
   readChange2: boolean = false;
   showDetail: boolean = false;
   showDetail2: boolean = false;
-  agent1Tool:any;
-  agent1Category:any;
-  agent2Tool:any;
-  agent2Category:any;
+  agent1Tool: any;
+  agent1Category: any;
+  agent2Tool: any;
+  agent2Category: any;
 
-  
-  constructor(private dataDictionaryService: DataDictionaryService) { 
+
+  constructor(private dataDictionaryService: DataDictionaryService) {
     this.dataDictionaryInfo();
   }
 
   ngOnInit() {
   }
-  
+
   async dataDictionaryInfo() {
     try {
       // Loads Agent , Data Component and Services
@@ -42,7 +43,7 @@ export class DatadictionaryComponent implements OnInit {
         for (var key in this.dictResponse.data) {
           this.agentDataSource.push(this.dictResponse.data[key]);
         }
-        
+
       }
     } catch (error) {
       console.log(error);
@@ -51,35 +52,35 @@ export class DatadictionaryComponent implements OnInit {
   async loadAgent1Info(selectedAgent1) {
     //console.log(selectedAgent1)
     //console.log(usersResponseData.length)
-    let usersResponseData1 = await this.dataDictionaryService.loadToolProperties(selectedAgent1.toolName,selectedAgent1.categoryName);
+    let usersResponseData1 = await this.dataDictionaryService.loadToolProperties(selectedAgent1.toolName, selectedAgent1.categoryName);
     //console.log(usersResponseData)
     if (usersResponseData1.data != undefined && usersResponseData1.status == "success") {
       this.showDetail = true;
-      this.agent1Tool=selectedAgent1.toolName;
-      this.agent1Category=selectedAgent1.categoryName;
+      this.agent1Tool = selectedAgent1.toolName;
+      this.agent1Category = selectedAgent1.categoryName;
       this.agent1TableData = usersResponseData1.data;
       //console.log(this.agent1Category);
 
-    } 
+    }
   }
   async loadAgent1Info2(selectedAgent2) {
     //console.log(selectedAgent2)
-    let usersResponseData2 = await this.dataDictionaryService.loadToolProperties(selectedAgent2.toolName,selectedAgent2.categoryName);
+    let usersResponseData2 = await this.dataDictionaryService.loadToolProperties(selectedAgent2.toolName, selectedAgent2.categoryName);
     if (usersResponseData2.data != undefined && usersResponseData2.status == "success") {
       //console.log(usersResponseData.data);
       this.showDetail2 = true;
-      this.agent2Tool=selectedAgent2.toolName;
-      this.agent2Category=selectedAgent2.categoryName;
+      this.agent2Tool = selectedAgent2.toolName;
+      this.agent2Category = selectedAgent2.categoryName;
       this.agent2TableData = usersResponseData2.data;
-    } 
+    }
   }
 
-  async getCorrelation(data1,data2){
+  async getCorrelation(data1, data2) {
     //console.log(data1,data2);
-    let usersResponseData3 = await this.dataDictionaryService.loadToolsRelationshipAndProperties(data1.toolName,data1.categoryName,data2.toolName,data2.categoryName);
+    let usersResponseData3 = await this.dataDictionaryService.loadToolsRelationshipAndProperties(data1.toolName, data1.categoryName, data2.toolName, data2.categoryName);
     console.log(usersResponseData3)
     if (usersResponseData3.data != undefined && usersResponseData3.status == "success") {
-      
+
     }
   }
 
