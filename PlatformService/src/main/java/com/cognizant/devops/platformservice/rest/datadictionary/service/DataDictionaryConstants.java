4f67d963aba370ad8a2af2de93663708d335ecab
gauravdeshmukh612 <gaurav.deshmukh2@cognizant.com>
2019-02-28 16:06:06 +0530

    164131121 -Business Mapping Edit data

--- PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/datadictionary/service/DataDictionaryConstants.java
+++ PlatformService/src/main/java/com/cognizant/devops/platformservice/rest/datadictionary/service/DataDictionaryConstants.java
@@ -18,7 +18,7 @@ package com.cognizant.devops.platformservice.rest.datadictionary.service;
 
 public interface DataDictionaryConstants {
 
-	String GET_TOOL_PROPERTIES_QUERY = "MATCH(n:__CategoryName__:__ToolName__:DATA) return keys(n) limit 1";
+	String GET_TOOL_PROPERTIES_QUERY = "MATCH(n:__CategoryName__:__ToolName__:DATA) return keys(n) order by n.inSightsTime desc limit 1";
 	String GET_TOOLS_RELATIONSHIP_QUERY = "MATCH (n:__StartToolCategory__:__StartToolName__)-[r]->"
 			+ "(m:__EndToolCategory__:__EndToolName__) return type(r),r LIMIT 1";
 	String UUID = "uuid";
